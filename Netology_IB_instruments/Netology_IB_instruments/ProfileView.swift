bb9884d3108fd4186be928a69ba04e724465bc8c
dzhamBool <dzhamBool@yandex.ru>
2022-05-04 16:00:36 +0300

    added project Netology_IB_Instruments'

---
+++ Netology_IB_instruments/Netology_IB_instruments/ProfileView.swift
@@ -0,0 +1,13 @@
+
+
+import UIKit
+
+class ProfileView: UIView {
+    @IBOutlet weak var signatureText: UITextView!
+    @IBOutlet weak var userPhoto: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+
+
+}
